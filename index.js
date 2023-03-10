a438c077fd8bc42caba8ee70e37a8adc14cd5f58
Vik Chadha <vikchadha@gmail.com>
2023-03-10 16:37:45 -0500

    Set up as NPM module

--- index.js
+++ index.js
@@ -1,29 +1,4 @@
-// Reverses a string.
-function reverse(string) {
-    return Array.from(string).reverse().join("");
-  }
-  
-// Returns true for a palindrome, false otherwise.
-function palindrome(string) {
-  let processedContent = string.toLowerCase();
-  return processedContent === reverse(processedContent);
-}
-
-// Defines a Phrase object.
-function Phrase(content) {
-  this.content = content;
-
-  // Returns content processed for palindrome testing
-  this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
-  }
-
-
-  // Returns true if the phrase is a palindrome, false otherwise.
-  this.palindrome = function palindrome() {
-    return this.processedContent() === reverse(this.processedContent());
-  }
-}
+module.exports = Phrase;
 
 // Adds `reverse` to all strings.
 String.prototype.reverse = function() {
