e7576374f5abfc4ae00cc6e99ada3b23328b87af
Vik Chadha <vikchadha@gmail.com>
2023-03-11 16:17:34 -0500

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -11,7 +11,20 @@ function Phrase(content) {
 
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.content.toLowerCase();
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content.
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
+    // let theLetters = [];
+    // const letterRegex = /[a-z]/i;
+    // Array.from(this.content).forEach(function(character) {
+    //   if (character.match(letterRegex)) {
+    //     theLetters.push(character);
+    //   }
+    // });
+    // return theLetters.join("");
   }
 
   // Returns true if the phrase is a palindrome, false otherwise.
